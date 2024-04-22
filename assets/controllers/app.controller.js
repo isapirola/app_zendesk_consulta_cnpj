061aad08b2f2dfab64b2131f19f272829940d5a4
IsaPirola <belapirola@gmail.com>
2024-04-22 18:25:26 -0300

    Fix api CORS error

--- assets/controllers/app.controller.js
+++ assets/controllers/app.controller.js
@@ -4,13 +4,16 @@ app.controller("AppController", [
     function ($scope, zendeskService) {
         var client = ZAFClient.init();
 
-        // Função para pesquisar dados do CNPJ
+        // Função para consultar CNPJ
         $scope.pesquisaCNPJ = function () {
+            // Chamar o serviço para consultar o CNPJ
             zendeskService
                 .consultarCnpj($scope.cnpj)
-                .then(function (response) {
-                    // Armazenar os dados do CNPJ na variável $scope.cnpjData
-                    $scope.cnpjData = response.data;
+                .then((response) => {
+                    // Manipular a resposta da API aqui
+                    console.log(response);
+                    // Exemplo: Armazenar a resposta em $scope.cnpjData
+                    $scope.cnpjData = response;
                 })
                 .catch(function (error) {
                     // Manipular erros aqui
--- assets/service/zendesk.service.js
+++ assets/service/zendesk.service.js
@@ -1,6 +1,16 @@
-app.service("zendeskService", function ($http) {
+app.service("zendeskService", function () {
     this.consultarCnpj = function (cnpj) {
-        var url = `https://receitaws.com.br/v1/cnpj/${cnpj}`;
-        return $http.get(url);
+        // var url = `https://receitaws.com.br/v1/cnpj/${cnpj}`;
+        // return $http.get(url);
+
+        const client = ZAFClient.init();
+
+        const options = {
+            url: `https://receitaws.com.br/v1/cnpj/${cnpj}`,
+            type: "GET",
+            cors: false,
+        };
+
+        return client.request(options);
     };
 });
