968971120216c28d98b91b507fbcaf262c6cf266
IsaPirola <belapirola@gmail.com>
2024-04-23 16:14:11 -0300

    Remove unused lines

--- assets/service/zendesk.service.js
+++ assets/service/zendesk.service.js
@@ -1,8 +1,5 @@
 app.service("zendeskService", function () {
     this.consultarCnpj = function (cnpj) {
-        // var url = `https://receitaws.com.br/v1/cnpj/${cnpj}`;
-        // return $http.get(url);
-
         const client = ZAFClient.init();
 
         const options = {
